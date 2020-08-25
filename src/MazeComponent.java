135b8cbec586650baf414dfaf0c681aaf8952eae
ViciousCupcake <22625405+ViciousCupcake@users.noreply.github.com>
2020-08-25 16:17:42 -0700

    update before restructure to draw on bufferedImage

--- src/MazeComponent.java
+++ src/MazeComponent.java
@@ -1,10 +1,16 @@
 package src;
 
+import java.awt.Image;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import javax.swing.Timer;
 
@@ -13,18 +19,21 @@ public class MazeComponent extends Component {
      *
      */
     private static final long serialVersionUID = 3712257156008500629L;
+    private Image virtualMemory;
     private Graphics g;
     private String operationMode = "null";
     Timer timer;
     boolean b;
     int index = 20;
+    int z = 0;
 
     public MazeComponent() {
+        virtualMemory = createImage(this.getWidth(), this.getHeight());
         timer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                index+=5;
-                if(index>50)
+                index += 5;
+                if (index > 50)
                     timer.stop();
                 repaint();
             }
@@ -37,7 +46,17 @@ public class MazeComponent extends Component {
     }
 
     public void paint(Graphics g) {
+        g.drawImage(virtualMemory, 0, 0, this);
         g.drawString("test1", 20, index);
+        virtualMemory = createImage(this.getWidth(), this.getHeight());
+        System.out.println(virtualMemory);
+        try {
+            ImageIO.write((RenderedImage) virtualMemory, "png", new File(++z + ".png"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(this.getWidth());
         /*this.g = g;
         g.drawRect(0, 0, 100, 100);
         // test();
